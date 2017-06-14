30a159a1de9bc937ae41ef4d2305803579e32f41
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2017-06-14 10:58:47 +0200

    url decoded

--- src/test/kotlin/no/vegvesen/vt/nvdb/tools/webhookproxy/Manual.kt
+++ src/test/kotlin/no/vegvesen/vt/nvdb/tools/webhookproxy/Manual.kt
@@ -5,10 +5,19 @@ import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.ContentType
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.HttpClients
+import java.net.URLDecoder
 
 fun main(args: Array<String>) {
     val post = HttpPost("https://mattermost.kantega.no/hooks/9humkpkwz3gh8fism451ajtzua")
     val (s, mattermost) = loadPayloads("apiv2SolrInvalidNumber.json")
+
+/*    val replace = mattermost.replace("(%\\d+\\w?)".toRegex()) {
+        try {
+            URLDecoder.decode(it.value, "UTF-8")
+        } catch(e: Exception) {
+            ""
+        }
+    }*/
     val payload = "payload=${mattermost}"
     post.entity = StringEntity(payload, ContentType.APPLICATION_FORM_URLENCODED)
     HttpClients.createDefault().use {
